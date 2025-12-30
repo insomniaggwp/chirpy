4dc826667f933ecf2f69b64d08e280e7865f02da
arif hidayat <arifhidayatbiz@gmail.com>
2025-12-30 10:47:31 +0700

    add middleware

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,22 @@
 import express from "express";
 const app = express();
-app.listen(8080);
+const PORT = 8080;
+const handlerReadiness = (req, res) => {
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send('OK');
+};
+const middlewareLogResponses = (req, res, next) => {
+    res.on("finish", () => {
+        const statusCode = res.statusCode;
+        if (statusCode >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+        }
+    });
+    next();
+};
+app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
+app.get("/healthz", handlerReadiness);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,28 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
-app.listen(8080)
+const PORT = 8080;
+
+const handlerReadiness = (req: Request, res: Response): void => {
+  res.set('Content-Type', 'text/plain; charset=utf-8')
+  res.send('OK')
+}
+
+const middlewareLogResponses = (req: Request, res: Response, next: NextFunction) => {
+  res.on("finish", () => {
+    const statusCode = res.statusCode;
+    if (statusCode >= 300) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+    }
+  });
+  next();
+}
+
+app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
+
+app.get("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
